e961bb5215864d49fcbff42ab99f00a94908dd9a
Angelo <angelo.lop.alv@gmail.com>
2022-10-08 10:11:36 -0300

    findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,7 +2,10 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
+
+import java.util.List;
 
 public class Program {
    public static void main(String[] args) {
@@ -13,5 +16,17 @@ public class Program {
       Seller seller = sellerDao.findById(3);
 
       System.out.println(seller);
+
+      System.out.println("\n=== TEST 2: seller findByDepartment ===");
+      Department department = new Department(2, null);
+      List<Seller> list = sellerDao.findByDepartment(department);
+
+      list.forEach(System.out::println);
+
+      System.out.println("\n=== TEST 2: seller findByDepartment ===");
+      List<Seller> listall = sellerDao.findAll();
+
+      listall.forEach(System.out::println);
+
    }
 }
