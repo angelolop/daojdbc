0b26d111d921e4cf65a0bd6a79faf69749ae4523
Angelo <angelo.lop.alv@gmail.com>
2022-10-08 09:25:04 -0300

    FindById implemented

--- src/db/DB.java
+++ src/db/DB.java
@@ -12,10 +12,11 @@ public class DB {
    public static Connection getConnection() {
       if (conn == null) {
          try {
+            Class.forName("com.mysql.jdbc.Driver");
             Properties props = loadProperties();
             String url = props.getProperty("dburl");
             conn = DriverManager.getConnection(url, props);
-         } catch (SQLException e) {
+         } catch (SQLException | ClassNotFoundException e) {
             throw new DbException(e.getMessage());
          }
       }
